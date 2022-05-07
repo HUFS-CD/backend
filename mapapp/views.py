e7714b781daaa98759e377a37ec04a70cb471dbf
smlee8806 <74856267+smlee8806@users.noreply.github.com>
2022-05-07 17:02:34 +0900

    first commit

---
+++ mapapp/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse
+
+from mapapp.models import Cctv
+from mapapp.serializers import CctvSerializer
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
+
+
+def index(request):
+    cctvs = Cctv.objects
+    return render(request, 'mapapp/index.html', {'cctv': cctvs})
+
+class CctvList(generics.ListCreateAPIView):
+    queryset = Cctv.objects.all()
+    serializer_class = CctvSerializer
+
+class CctvDetailList(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cctv.objects.all()
+    serializer_class = CctvSerializer
