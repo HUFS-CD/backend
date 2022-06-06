3a8fd818e2270c400d11b7551eaa173837fa9c38
nhoseok <hoseok0320@gmail.com>
2022-06-06 18:54:16 +0900

    update

--- mapapp/views.py
+++ mapapp/views.py
@@ -15,19 +15,29 @@ from rest_framework.response import Response
 
 def index(request):
     cctvs = Cctv.objects.all()
-    test_difference= []
-    five_difference= []
+    cctv_list = list(cctvs)
+    my_array = []
     test_long = 127.06004762649577
     test_lat = 37.59644996896789
+    end_lng = 127.0654903650288
+    end_lat = 37.59585576408115
+    
+    while True:
+        test_difference = []
+        for cctv in cctv_list:
+            g = math.sqrt(math.pow(float(cctv.latitude) - float(test_lat), 2) + math.pow(float(cctv.longtitude) - float(test_long), 2))
+            h = math.sqrt(math.pow(float(cctv.latitude) - float(end_lat), 2) + math.pow(float(cctv.longtitude) - float(end_lng), 2))
+            test_difference.append([cctv.id, g, cctv.address, cctv.latitude, cctv.longtitude, h])
+        test_difference.sort(key=lambda x: x[1] + x[5])
+        if float(test_difference[0][1]) - float(test_difference[0][5]) < 0.001:
+            break
+        else:
+            test_long = test_difference[0][4]
+            test_lat = test_difference[0][3]
+            my_array.append(test_difference[0])
+            del cctv_list[test_difference[0][0]]
 
-
-    for cctv in cctvs:
-        test_difference.append([cctv.id, math.sqrt(math.pow(float(cctv.latitude) - test_lat, 2) + math.pow(float(cctv.longtitude) - test_long, 2)), cctv.address])
-
-    test_difference.sort(key=lambda x: x[1])
-    test_difference = test_difference[:5]
-
-    return render(request, 'mapapp/index.html', {'cctvs': test_difference})
+    return render(request, 'mapapp/index.html', {'cctvs': my_array})
 
 def main(request):
     cctvs = Cctv.objects.all()
