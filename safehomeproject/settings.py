b4a7c2dcedf3536869c9b2fe54205e85d01fc2e7
nhoseok <hoseok0320@gmail.com>
2022-06-14 17:08:58 +0900

    up

--- safehomeproject/settings.py
+++ safehomeproject/settings.py
@@ -81,19 +81,27 @@ WSGI_APPLICATION = 'safehomeproject.wsgi.application'
 # RDS
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cctv-database',
-        'USER': 'admin',
-        'PASSWORD': '!WhdgkqtjfrP9',
-        'HOST': 'cctv-database.cwpl95oqcakk.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'cctv-database',
+#         'USER': 'admin',
+#         'PASSWORD': '!WhdgkqtjfrP9',
+#         'HOST': 'cctv-database.cwpl95oqcakk.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
