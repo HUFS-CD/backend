63682f0831b7d5f226d06b6f4b074d29d7aedcef
nhoseok <hoseok0320@gmail.com>
2022-06-14 15:46:30 +0900

    change setting cors

--- safehomeproject/settings.py
+++ safehomeproject/settings.py
@@ -139,10 +139,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:5500', 
     'http://localhost:5500', 
-    'cctv-database.cwpl95oqcakk.ap-northeast-2.rds.amazonaws.com:3306/cctv',
     'http://127.0.0.1:3000',
     'http://localhost:3000',
-    '*'
+    'http://127.0.0.1:3306',
+    'http://localhost:3000',
     )
 CORS_ALLOW_CREDENTIALS = True
 
